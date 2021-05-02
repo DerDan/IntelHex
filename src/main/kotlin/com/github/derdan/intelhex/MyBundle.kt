4888746f7d1f5f778f19985360044a1d9a63a84e
GitHub Action <action@github.com>
2021-05-02 14:02:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/derdan/intelhex/MyBundle.kt
+++ src/main/kotlin/com/github/derdan/intelhex/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.derdan.intelhex
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/derdan/intelhex/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/derdan/intelhex/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.derdan.intelhex.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.derdan.intelhex.MyBundle
 
 class MyApplicationService {
 
